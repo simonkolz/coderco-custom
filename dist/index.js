b3d175248a5eace2bba5e1ab9d5155649eff5ef7
simonkolz <simonkolz11@gmail.com>
2026-01-09 20:25:55 +0000

    Build action with ncc

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 170:
+/***/ 808:
 /***/ ((module) => {
 
 module.exports = eval("require")("actions/core");
@@ -48,7 +48,7 @@ module.exports = eval("require")("actions/core");
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const core = __nccwpck_require__(170)
+const core = __nccwpck_require__(808)
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
